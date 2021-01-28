677cfd6d950a651e1dedf1b8153fc4f1713428e7
Paul Fries <paul@paulfries.com>
2021-01-28 12:57:21 -0700

    Converting to RDS

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,12 @@ export SECRET_KEY_BASE=`rake secret`
 
 export RAILS_ENV="production";
 
+export RDS_DATABASE="warscorecard";
+export RDS_USERNAME="postgres";
+export RDS_PASSWORD="1qaz!QAZ";
+export RDS_HOST="warscorecard.cqay2i8lc1aa.us-east-2.rds.amazonaws.com";
+export RAILS_ENV="production";
+
 rake db:create db:migrate db:seed
 
 exec rails s -b 0.0.0.0 -p 3000
